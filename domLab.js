a00360f86f621d667ae5d4884be1222b62d4f9f1
sandra stark <stark.sandy@gmail.com>
2014-11-20 18:52:17 -0800

    problems 1-3

---
+++ domLab.js
@@ -0,0 +1,31 @@
+
+//1.) When someone clicks the #colorDiv change its background color to blue.
+var div = document.getElementById( 'colorDiv' );
+div.onclick = function() {
+  this.style.backgroundColor = 'blue';
+  };
+
+
+//2.) When someone clicks the #toggleMe toggle the background color between white and red.
+var twoColors = document.getElementById("toggleMe");
+
+var white = true;
+twoColors.onclick= function () {
+    if (white) {
+      twoColors.style.backgroundColor = "red";
+      }
+    else {
+        twoColors.style.backgroundColor = "white";
+    }
+  white = !white;
+};  
+
+
+//3.) When somone clicks the #countMe div display the number of times it has been clicked.
+var counter = 0;
+document.getElementById("countMe").onclick= function(event){
+ counter +=1;
+ document.getElementById("countMe").innerHTML = "click" + counter;
+};
+
+//Attach a click listener to #greetingDiv and grab the #nameField from the input tag. Use the #nameFieldto display Hello, with the entered name.
