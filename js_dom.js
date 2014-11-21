e30da8a92fb4f9070e60a2e16164cb7219e0d149
Alex Fulsome <alexfulsome@gmail.com>
2014-11-21 09:43:27 -0800

    dom homework

---
+++ js_dom.js
@@ -0,0 +1,29 @@
+var click =
+document.getElementById('colorDiv');
+
+click.addEventListener('click',
+function () { colorDiv.style.backgroundColor = "blue"; }, false);
+
+var toggle = document.getElementById('toggleMe');
+
+var white = true;
+toggle.onclick = function (event) {
+  if (white) {
+    toggle.style.backgroundColor = "white";
+  } else {
+    toggle.style.backgroundColor = "red";
+  }
+  white = !white;
+};
+
+var clickCount = 0;
+document.getElementById("countMe").onclick= function(event){
+clickCount +=1;
+document.getElementById("countMe").innerHTML = "click: " + clickCount;
+};
+
+var greeting = document.getElementById('greetingDiv').onclick= function(event) {
+click.addEventListener('click', function(event){
+  
+  
+});
