1d47bf38721104475ce5e5c2b8288f68d750ad14
Angelo Cisneros <a.e.cisneros@gmail.com>
2014-11-30 21:36:38 -0800

    Add answers.js, forgot to add before pushing to github

--- answers.js
+++ answers.js
@@ -15,6 +15,20 @@ toggleMe.onclick =  function() {
   toggleCounter += 1;
 };
 
+// or Dels Solution
+
+      // in CSS
+      .redDiv {
+          background-color: red;
+      }
+
+      // in script
+      var toggleMe = document.getElementById("toggleMe");
+
+      toggleMe.onclick = function () {
+          toggleMe.classList.toggle(".redDiv");
+      }
+
 
 // 3.) When somone clicks the `#countMe` div display the number of times it has been clicked.
 var countMe = document.querySelector("#countMe");
@@ -24,6 +38,14 @@ countMe.onclick = function(event) {
   countMe.innerHTML = "click me " + clickCounter;
 };
 
+    // or Del's solution
+    var countMe = document.getElementById("countMe");
+    var clickCount = 0;
+    counteMe.onclick = function () {
+        clickCount += 1;
+        countMe.innerHTML = clickCount;
+    };
+
 // 4.) Attach a `click` listener to `#greetingDiv` and grab the `#nameField` from the `input` tag. Use the `#nameField`to display `Hello, ` with the entered name.
 
 var greetingDiv = document.querySelector("#greetingDiv");
@@ -32,3 +54,13 @@ var nameField = document.querySelector("#nameField");
 greetingDiv.onclick = function(event){
   nameField.value = "Hello, " + nameField.value;
 };
+
+// or Del's
+
+    var greetingDiv = document.getElementById("greetingDiv");
+    var nameField = document.getElementById("nameField");
+
+    greetingDiv.onclick = function () {
+        var name = nameField.value;
+       su greetingDiv.innerHTML = "Hello, " + name;
+    };
